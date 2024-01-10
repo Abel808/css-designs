7798a8d97f620c631efa67050ac8000428aca801
Abel808 <able.kodes@gmail.com>
2024-01-10 17:12:20 +0530

    added to&from

---
+++ gsap/to & from/app.js
@@ -0,0 +1,23 @@
+
+
+
+gsap.to('.prog',{
+    width:"100%",
+    duration:0.8,
+})
+
+
+gsap.from('h1',{
+    duration:2.5,
+    ease: "elastic.out(1,0.3)",
+    y: -150
+})
+
+
+
+gsap.to('.prog',{
+    height:"100vh",
+    delay:1,
+    top:0,
+    duration:1.2
+})
